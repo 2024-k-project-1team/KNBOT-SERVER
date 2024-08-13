72b1bf59542b1e8e396d149e250857f60def5d3d
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2024-08-13 15:25:52 +0900

    :bug: Delete unused file

--- src/main/java/org/likelion/newsfactbackend/company/repository/MediaCompanyRepository.java
+++
@@ -1,7 +0,0 @@
-package org.likelion.newsfactbackend.company.repository;
-
-import org.likelion.newsfactbackend.company.domain.MediaCompany;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MediaCompanyRepository extends JpaRepository<MediaCompany,Long> {
-}
